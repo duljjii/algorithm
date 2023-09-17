7ee8cdc0afbde05b4dcfefc21c6afde2afdf455d
duljjii <131976757+duljjii@users.noreply.github.com>
2023-09-18 00:34:50 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,56 +1,44 @@
-import java.util.*;
 import java.io.*;
-public class Main {
-    public static int N, M;
+import java.util.*;
 
-    private static int stoi(String str){
-        return Integer.parseInt(str);
-    }
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = stoi(st.nextToken());
-        M = stoi(st.nextToken());
-        List<int[]> lst = new ArrayList<int[]>();
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int num = stoi(st.nextToken());
-            int gold = stoi(st.nextToken());
-            int silver = stoi(st.nextToken());
-            int bronze = stoi(st.nextToken());
-            lst.add(new int[]{num, gold, silver, bronze});
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] countries = new int[N][4]; // 국가 정보를 저장할 배열
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            countries[i][0] = Integer.parseInt(st.nextToken()); // 국가 번호
+            countries[i][1] = Integer.parseInt(st.nextToken()); // 금메달 수
+            countries[i][2] = Integer.parseInt(st.nextToken()); // 은메달 수
+            countries[i][3] = Integer.parseInt(st.nextToken()); // 동메달 수
         }
-        Collections.sort(lst, (a, b)->{
-            if(a[1] == b[1]){
-                if(a[2] == b[2]){
-                    if(a[3] == b[3]){
-                        return 0;
-                    }
+
+        Arrays.sort(countries, (a, b) -> {
+            if (a[1] == b[1]) {
+                if (a[2] == b[2]) {
                     return b[3] - a[3];
                 }
                 return b[2] - a[2];
             }
             return b[1] - a[1];
         });
-        if(lst.get(0)[0] == M){
-            System.out.println(1);
-            return;
-        }
-        int idx = 1;
-        for(int i=1; i<lst.size(); i++){
-            int gold = lst.get(i)[1];
-            int silver = lst.get(i)[2];
-            int bronze = lst.get(i)[3];
-            if(lst.get(i-1)[1] != gold || lst.get(i-1)[2] != silver || lst.get(i-1)[3] != bronze){
-                idx++;
-            }
 
-            if(lst.get(i)[0] == M){
-                System.out.println(idx);
+        int rank = 1;
+        for (int i = 0; i < N; i++) {
+            if (countries[i][0] == K) {
+                System.out.println(rank);
                 return;
             }
-
+            if (i > 0 && Arrays.equals(countries[i], countries[i - 1])) {
+                rank--;
+            }
+            rank++;
         }
     }
 }
