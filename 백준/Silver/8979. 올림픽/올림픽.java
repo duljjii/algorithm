e80ef3401367c5ffdccc1d900102ef48ad2bec39
duljjii <131976757+duljjii@users.noreply.github.com>
2023-09-18 00:27:06 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,51 +1,56 @@
-import java.net.Inet4Address;
 import java.util.*;
 import java.io.*;
+public class Main {
+    public static int N, M;
 
-class Main {
+    private static int stoi(String str){
+        return Integer.parseInt(str);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] player = new int[N+1][4];
-
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        List<int[]> lst = new ArrayList<int[]>();
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            player[i][0] = Integer.parseInt(st.nextToken());
-            player[i][1] = Integer.parseInt(st.nextToken());
-            player[i][2] = Integer.parseInt(st.nextToken());
-            player[i][3] = Integer.parseInt(st.nextToken());
+            int num = stoi(st.nextToken());
+            int gold = stoi(st.nextToken());
+            int silver = stoi(st.nextToken());
+            int bronze = stoi(st.nextToken());
+            lst.add(new int[]{num, gold, silver, bronze});
         }
-
-        Arrays.sort(player, (a, b)->{
+        Collections.sort(lst, (a, b)->{
             if(a[1] == b[1]){
                 if(a[2] == b[2]){
+                    if(a[3] == b[3]){
+                        return 0;
+                    }
                     return b[3] - a[3];
                 }
                 return b[2] - a[2];
             }
             return b[1] - a[1];
         });
-
-        int idx = 1;
-        if(player[0][0] == M){
-            System.out.println(idx);
+        if(lst.get(0)[0] == M){
+            System.out.println(1);
             return;
         }
-        for(int i=1; i<N+1; i++){
-            if(player[i][0] == M){
+        int idx = 1;
+        for(int i=1; i<lst.size(); i++){
+            int gold = lst.get(i)[1];
+            int silver = lst.get(i)[2];
+            int bronze = lst.get(i)[3];
+            if(lst.get(i-1)[1] != gold || lst.get(i-1)[2] != silver || lst.get(i-1)[3] != bronze){
+                idx++;
+            }
+
+            if(lst.get(i)[0] == M){
                 System.out.println(idx);
-                break;
+                return;
             }
-            if(player[i][0] == player[i-1][0] && player[i][1] == player[i-1][1] && player[i][2] == player[i-1][2]){
-                continue;
-            }
-            idx++;
+
         }
-
-
     }
-
 }
