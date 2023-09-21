1726d00ad118376f0c94786ccaffd84c5ad487fe
duljjii <131976757+duljjii@users.noreply.github.com>
2023-09-22 07:11:31 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 372 ms, Memory: 27196 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        // 배열 입력 받기
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int result = findLargestSquare(n, m, arr);
+
+        System.out.println(result);
+    }
+
+    public static int findLargestSquare(int n, int m, int[][] arr) {
+        int[][] dp = new int[n][m];
+        int maxSize = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = arr[i][j];
+                } else if (arr[i][j] == 1) {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                }
+
+                maxSize = Math.max(maxSize, dp[i][j]);
+            }
+        }
+
+        return maxSize * maxSize;
+    }
+}
